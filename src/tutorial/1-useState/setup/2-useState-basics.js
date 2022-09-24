53fc1f8bd067503052bb1c2c67cd2d4e9dc64468
Tyler D <62620034+TylerDouberly@users.noreply.github.com>
2022-09-24 17:51:51 -0400

    update useState practice

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
+// useState - function 
+// is named so you must use curly braces  { useState }
+// used for all hooks
+// start with use
+// component name must be uppercase
+// must be invoked in  the function/component body
+// cannon call conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.timeLog(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handle = useState(1)[1];
+  // console.log(value, handler);
+  // name , set name
+  
+  const [ text, setText] = useState('random title')
+
+  const handleClick = () =>{
+    if (text === 'random title') {
+      
+      setText('hello world');
+      
+    }else{
+      setText('random title');
+    }
+  }
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}>
+      change title
+    </button>
+  </React.Fragment>
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { data } from '../../../data';
+import { data } from '../../../data';//realtive path
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+  const [people, setPeople] = React.useState(data); // can use instead of import React, { useState } from 'react';
+  
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
 
+  }; 
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove
+            </button>
+
+          </div>
+      );
+    })}
+    <button className='btn' onClick={() => setPeople([])}> 
+      Clear Items
+    </button>
+  </>
+  );
+};
+// can use in line () => setPeople([])
 export default UseStateArray;
