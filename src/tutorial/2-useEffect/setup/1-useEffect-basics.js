240a4f8dbc6731a75ab8ec18099a5a8e8bdeae3b
Tyler D <62620034+TylerDouberly@users.noreply.github.com>
2022-09-27 19:01:53 -0400

    completed 2-useEffect
    completed 2-useEffect, basics, cleanup, fetch data.

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  const [value, setValue] = useState(0)
 
-export default UseEffectBasics;
+  useEffect(() => {
+    console.log('call useEffect')
+    //can use if/else within the callback
+    if (value >= 1) {
+      document.title = `New Messages(${value})`
+    }
+  }, [value])
+  // if you want it to only run on the initial render use an empty dependency
+  // you can also have as many use effects as you would like
+  useEffect(() => {
+    console.log('hello world')
+  }, [])
+
+  console.log('render componet')
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        Click me
+      </button>
+    </>
+  )
+}
+
+export default UseEffectBasics
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users'
+
+//second argument
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
-};
+  const [users, setUsers] = useState([])
 
-export default UseEffectFetchData;
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    //setUsers(users) will cause a infinite loop with triggering rerender
+    //console.log(users)
+    setUsers(users)
+  }
+  // useEffect cannot be async/ cannot return a promise
+  useEffect(() => {
+    getUsers()
+  }, []) //do'nt forget to add dependency
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default UseEffectFetchData
