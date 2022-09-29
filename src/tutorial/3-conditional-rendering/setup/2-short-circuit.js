b07a27eec246649270d3522960755c2462143f59
Tyler D <62620034+TylerDouberly@users.noreply.github.com>
2022-09-28 21:12:26 -0400

    completed 3-conditional-rendering
    1-multiple returns, 2- short-circuits, and 3-show-hide

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
-// ternary operator
+// ternary operator gives three possible outcomes
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
 
-  return <h2>short circuit</h2>;
-};
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value :{secondValue}</h1> */}
+      {/* {if(){
+    console.log('hello world');
+  }} */}
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>There is an error...</p>
+      ) : (
+        <div>
+          <h2>There is no error...</h2>
+        </div>
+      )}
+      {/* {text && <h1>Hello WOrld</h1>}
+      {!text && <h1>Hello WOrld</h1>} */}
+    </>
+  )
+}
 
-export default ShortCircuit;
+export default ShortCircuit
